8215907912ecf10efa001af8611f5015de0413a4
Abduvokhid <wahid@sju.ac.kr>
2018-05-17 16:41:49 +0900

    Initial Commit

---
+++ CompGraps_ClassCode_3DLighting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CompGraps_ClassCode_3DLighting
+//
+//  Created by mac on 5/17/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
